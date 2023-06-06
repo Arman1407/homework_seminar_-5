d4ab62d7c7e8f528b7c4ff8c8b631d90db19174f
Arman_Dzhambulov <arman1407@ya.ru>
2023-06-06 09:20:33 +0300

    task#38

---
+++ task#38/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 38: 
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+
+void InputArray(double[] array)
+{
+    int end = 100, begin = 0;
+    for (int i = 0; i < array.Length; i++)
+    array[i] = Math.Round(new Random().NextDouble() * (end - begin) + begin, 2);
+}
+
+//void Diff(int[] array);
+int[] diff(int[] result, int[] imax, int[] imin)
+{
+    // int imax = 0;
+    // int imin = 0;
+
+    for (int i = 0; i < array.Length; i++);
+    result[i] = imax[i] - imin[i];
+    return result;
+}
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+double[] array = new double[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(": ", array)}]");
+// Diff(araay);
+Console.WriteLine($"Результат: {string.Join(Diff(imax) - (imin) = (imax - imin))}");
+
+// Console.WriteLine($"Конечный массив: {string.Join(", ", proizvedenie(array, result))}");
+
+
+
